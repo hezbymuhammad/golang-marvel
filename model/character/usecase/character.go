a59a407850fabd4b90c42fc50d29a7c2efabe96a
hezbymuhammad <hezbymuhammad@gmail.com>
2021-07-19 08:59:17 +0700

    define usecase

---
+++ model/character/usecase/character.go
@@ -0,0 +1,63 @@
+package usecase
+
+import (
+	"context"
+	"time"
+	"fmt"
+
+	"hezbymuhammad/golang-marvel-demo/domain"
+)
+
+type characterUsecase struct {
+	characterReadRepo   domain.CharacterReadRepository
+	characterWriteRepo  domain.CharacterWriteRepository
+	contextTimeout      time.Duration
+}
+
+func NewCharacterUsecase(crr domain.CharacterReadRepository, cwr domain.CharacterWriteRepository, timeout time.Duration) domain.CharacterUsecase {
+	return &characterUsecase{
+		characterReadRepo: crr,
+		characterWriteRepo: cwr,
+		contextTimeout: timeout,
+	}
+}
+
+func (cu *characterUsecase) Fetch(c context.Context, page int) ([]int, error) {
+        ctx, cancel := context.WithTimeout(c, cu.contextTimeout)
+	defer cancel()
+
+        go func() {
+                err := cu.characterWriteRepo.StoreByPage(ctx, page)
+                if(err != nil) {
+                        fmt.Errorf("Error saving page %d", page)
+                }
+        }()
+
+        res, err := cu.characterReadRepo.Fetch(ctx, page)
+
+        if(err != nil) {
+                return nil, err
+        }
+
+        return res, nil
+}
+
+func (cu *characterUsecase) GetByID(c context.Context, id int64) (domain.Character, error) {
+        ctx, cancel := context.WithTimeout(c, cu.contextTimeout)
+	defer cancel()
+
+        go func() {
+                err := cu.characterWriteRepo.StoreByID(ctx, id)
+                if(err != nil) {
+                        fmt.Errorf("Error saving id %d", id)
+                }
+        }()
+
+        res, err := cu.characterReadRepo.GetByID(ctx, id)
+
+        if(err != nil) {
+                return domain.Character{}, err
+        }
+
+        return res, nil
+}
---
+++ model/character/usecase/character_test.go
@@ -0,0 +1,109 @@
+package usecase_test
+
+import (
+        "context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/mock"
+        "github.com/stretchr/testify/suite"
+
+	"hezbymuhammad/golang-marvel-demo/domain"
+	"hezbymuhammad/golang-marvel-demo/domain/mocks"
+	"hezbymuhammad/golang-marvel-demo/model/character/usecase"
+)
+
+type CharacterUsecaseTestSuite struct {
+	suite.Suite
+	usecase       domain.CharacterUsecase
+	readRepo      *mocks.CharacterReadRepository
+	writeRepo     *mocks.CharacterWriteRepository
+}
+
+func TestCharacterUsecase(t *testing.T) {
+	suite.Run(t, new(CharacterUsecaseTestSuite))
+}
+
+func (s *CharacterUsecaseTestSuite) SetupTest() {
+	s.readRepo = new(mocks.CharacterReadRepository)
+	s.writeRepo = new(mocks.CharacterWriteRepository)
+        s.usecase = usecase.NewCharacterUsecase(s.readRepo, s.writeRepo, time.Second*2)
+}
+
+func (s *CharacterUsecaseTestSuite) TestSuccessFetch() {
+        arr := []int{1, 2, 3}
+        s.readRepo.On("Fetch", mock.Anything, 1).Return(arr, nil).Once()
+        s.writeRepo.On("StoreByPage", mock.Anything, 1).Return(nil).Once()
+
+        res, err := s.usecase.Fetch(context.Background(), 1)
+        s.Assert().Equal(res, arr)
+        s.Assert().Equal(err, nil)
+}
+
+func (s *CharacterUsecaseTestSuite) TestFailedFetch() {
+        arr := []int{1, 2, 3}
+        dummy_err := errors.New("SomeError")
+        s.readRepo.On("Fetch", mock.Anything, 1).Return(arr, dummy_err).Once()
+        s.writeRepo.On("StoreByPage", mock.Anything, 1).Return(nil).Once()
+
+        res, err := s.usecase.Fetch(context.Background(), 1)
+        s.Assert().Nil(res)
+        s.Assert().Equal(err, dummy_err)
+}
+
+func (s *CharacterUsecaseTestSuite) TestFailedStoreByPage() {
+        arr := []int{1, 2, 3}
+        s.readRepo.On("Fetch", mock.Anything, 1).Return(arr, nil).Once()
+        s.writeRepo.On("StoreByPage", mock.Anything, 1).Return(errors.New("SomeError")).Once()
+
+        res, err := s.usecase.Fetch(context.Background(), 1)
+        s.Assert().Equal(res, arr)
+        s.Assert().Equal(err, nil)
+}
+
+func (s *CharacterUsecaseTestSuite) TestSuccessGetByID() {
+        record := domain.Character{
+                ID: 1,
+                Name: "Lorem",
+                Description: "Lorem",
+                FetchedAt: time.Now(),
+        }
+        s.readRepo.On("GetByID", mock.Anything, int64(1)).Return(record, nil).Once()
+        s.writeRepo.On("StoreByID", mock.Anything, int64(1)).Return(nil).Once()
+
+        res, err := s.usecase.GetByID(context.Background(), int64(1))
+        s.Assert().Equal(res, record)
+        s.Assert().Equal(err, nil)
+}
+
+func (s *CharacterUsecaseTestSuite) TestFailedGetByID() {
+        record := domain.Character{
+                ID: 1,
+                Name: "Lorem",
+                Description: "Lorem",
+                FetchedAt: time.Now(),
+        }
+        dummy_err := errors.New("SomeError")
+        s.readRepo.On("GetByID", mock.Anything, int64(1)).Return(record, dummy_err).Once()
+        s.writeRepo.On("StoreByID", mock.Anything, int64(1)).Return(nil).Once()
+
+        res, err := s.usecase.GetByID(context.Background(), int64(1))
+        s.Assert().Equal(res, domain.Character{})
+        s.Assert().Equal(err, dummy_err)
+}
+
+func (s *CharacterUsecaseTestSuite) TestFailedStoreByID() {
+        record := domain.Character{
+                ID: 1,
+                Name: "Lorem",
+                Description: "Lorem",
+                FetchedAt: time.Now(),
+        }
+        s.readRepo.On("GetByID", mock.Anything, int64(1)).Return(record, nil).Once()
+        s.writeRepo.On("StoreByID", mock.Anything, int64(1)).Return(errors.New("SomeError")).Once()
+
+        res, err := s.usecase.GetByID(context.Background(), int64(1))
+        s.Assert().Equal(res, record)
+        s.Assert().Equal(err, nil)
+}
